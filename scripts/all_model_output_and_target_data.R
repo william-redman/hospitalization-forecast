17b6fd7b202b874261730fdb9fda2f3882585fbd
ai4castinghub-admin <4castinghub@uoguelph.ca>
2025-04-27 23:31:26 -0400

    updated eval script

--- scripts/all_model_output_and_target_data.R
+++ scripts/all_model_output_and_target_data.R
@@ -81,7 +81,7 @@ all_model_data <- bind_rows(
 )
 
 all_model_data <- all_model_data %>%
-  mutate(values = ifelse(values < 0, 0, ceiling(values)))
+  mutate(value = ifelse(value < 0, 0, ceiling(value)))
 
 # Save the combined data to a CSV file
 write_csv(all_model_data, "auxiliary-data/concatenated_model_output.csv")
